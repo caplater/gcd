2f1248c6dde0d162eb33fa642761138c341bc1f7
ab3189 <TcqbqB4k%4wnwd>
2018-12-05 21:34:26 -0500

    Initial Commit

---
+++ gcd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gcd
+//
+//  Created by Charles Plater on 12/5/18.
+//  Copyright © 2018 Charles Plater. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
