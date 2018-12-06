e2ac1d8aecef70e41a2cff909f80fda04fe1ad12
ab3189 <TcqbqB4k%4wnwd>
2018-12-05 22:12:26 -0500

    Working Code

--- gcd/main.swift
+++ gcd/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 
+func gcd(_ m: Int, _ n: Int) -> Int {
+
+    let a = max(m,n)
+    let b = min(m,n)
+    
+    let remainder = a % b;
+    
+    if remainder != 0 {
+        return gcd(b,remainder)
+    } else {
+        return b
+    }
+}
+
+if CommandLine.arguments.count != 3 {
+    print("Usage: gcd(a,b) ", CommandLine.arguments.count)
+    exit(1)
+}
+
+let a = Int(CommandLine.arguments[1])
+let b = Int(CommandLine.arguments[2])
+let div = gcd(a!,b!)
+
+print ((gcd(a!,b!)))
